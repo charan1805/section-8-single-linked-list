006c86f2e1a9d1efd347c6afd22d93260ac18af9
charan1805 <sreecharanmudhiraj@gmail.com>
2023-07-28 20:54:29 +0530

    First Class

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace SignlyListedLists
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        public static void runApp()
+        {
+            SingleLinkedList list = new SingleLinkedList();
+            Node n = new Node();
+            n.Data = "root";
+            list.add(n);
+            for (int i = 0; i < 10; i++)
+            {
+                Node node = new Node();
+                node.Data = Convert.ToString(i);
+                list.add(node);
+            }
+
+            Console.Write("length of singly linked list=" + list.getLengthOfList());
+
+            n = list.getRoot();
+            while (n != null)
+            {
+                Console.WriteLine(n.Data);
+                n = list.getNext();
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
